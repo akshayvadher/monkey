3e1cb6211f8c8a0f58423d34f916cf075b4afc13
Akshay Vadher <akshay@incubyte.co>
2024-04-24 23:17:13 +0530

    Ch 1 Lexing: Basic lexer tests passing

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier + Literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
