ec3db4f81123bdac27485b71b5792db577c00745
Akshay Vadher <akshay@incubyte.co>
2024-05-10 16:21:31 +0530

    Ch 3 Evaluation: 3.10 Function calling

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -196,6 +196,72 @@ func TestBangOperator(t *testing.T) {
 	}
 }
 
+func TestFunctionObject(t *testing.T) {
+	input := "fn(x) { x + 2 };"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("Object is not a function. Got %T (%+v)", evaluated, evaluated)
+	}
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong parameters %+v", fn.Parameters)
+	}
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("patameter is not 'x'. Got %q", fn.Parameters[0])
+	}
+	expectedBody := "(x + 2)"
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("body is not %q. Got %q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionCall(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 6);", 11},
+		{"let add = fn(x, y) { x + y; }; add(5, 6 + 1);", 12},
+		{"let add = fn(x, y) { x + y; }; add(5, add(4, 7));", 16},
+		{"fn(x) { x; }(5)", 5},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%s", tt.input), func(t *testing.T) {
+			testIntegerObject(t, testEval(tt.input), tt.expected)
+		})
+	}
+}
+
+func TestClosures(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{`
+let newAdder = fn(x) {
+	fn(y) { x + y }
+};
+let addTwo = newAdder(2);
+addTwo(2)
+`, 4},
+		{`
+let add = fn(a, b) { a + b };
+let sub = fn(a, b) { a - b };
+let apply = fn(a, b, func) { func(a, b) };
+apply(2, apply(3, 4, sub), add);
+`, 1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%s", tt.input), func(t *testing.T) {
+			testIntegerObject(t, testEval(tt.input), tt.expected)
+		})
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
