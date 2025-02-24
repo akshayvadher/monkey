56e0f864f38547ae720ab518ebffd6dc9143e6a5
Akshay Vadher <akshay@incubyte.co>
2025-02-24 16:34:55 +0530

    [Compiler] Ch 2: (Array | Hash)[index] done

--- code/code.go
+++ code/code.go
@@ -35,6 +35,7 @@ const (
 
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -63,6 +64,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
